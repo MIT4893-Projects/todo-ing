453d01d925d3076ea354d62c41e80dea35df3689
MIT4893-Projects <minhngoc2891211@gmail.com>
2024-04-05 00:18:38 +0700

    add responsive layout

--- src/components/Containers/NoteContainer.jsx
+++ src/components/Containers/NoteContainer.jsx
@@ -5,7 +5,9 @@ NoteContainer.propTypes = noteContainerPropTypes;
 export default function NoteContainer({ children }) {
   return (
     <div className="note-container container-fluid p-2">
-      <div className="row row-cols-3 g-2">{children}</div>
+      <div className="row row-cols-sm-2 row-cols-lg-3 row-cols-xl-4 g-2">
+        {children}
+      </div>
     </div>
   );
 }
