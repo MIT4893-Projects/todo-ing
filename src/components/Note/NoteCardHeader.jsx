d52146c8b42b00c39fb4cd38d1109c21afcd11b1
MIT4893-Projects <minhngoc2891211@gmail.com>
2024-04-11 22:12:45 +0700

    Fix note header text overflow

--- src/components/Note/NoteCardHeader.jsx
+++ src/components/Note/NoteCardHeader.jsx
@@ -3,5 +3,16 @@ import { noteCardHeaderPropTypes } from "./propTypes";
 NoteCardHeader.propTypes = noteCardHeaderPropTypes;
 
 export default function NoteCardHeader({ header }) {
-  return <h3 className="h3 card-title m-0">{header}</h3>;
+  return (
+    <h3
+      className="h3 card-title m-0"
+      style={{
+        textOverflow: "ellipsis",
+        whiteSpace: "nowrap",
+        overflow: "hidden",
+      }}
+    >
+      {header}
+    </h3>
+  );
 }
